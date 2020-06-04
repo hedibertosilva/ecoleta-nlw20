cf47ab1c66c37a4fabec401a59f571d90d836648
Hediberto C Silva <hediberto.silva@logicasistemas.com.br>
2020-06-04 18:07:54 -0300

    NLW#2 Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
+
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  response.json([
-    'hediberto',
-    'cavalcante',
-    'silva',
-    'teste']);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(
+  path.resolve(__dirname, '..', 'uploads')
+));
 
 app.listen(3333);
