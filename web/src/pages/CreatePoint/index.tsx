dd6842ae0f44d2f5be13b1123065ddfff30b96ba
Hediberto C Silva <hediberto.silva@logicasistemas.com.br>
2020-06-04 20:38:24 -0300

    Pages Home, CreatePoint

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+      </header>
+    </div>
+  )
+};
+
+export default CreatePoint;
